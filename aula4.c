a46aa561c1ccd87673709219b41dfec3359ab315
Ricardo Santos <ricardovieirads@outlook.com>
2025-09-26 23:58:45 -0300

    Add files via upload

---
+++ aula4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main(){
+
+
+printf(" ola mundo");
+
+int numeros[5]={5,15,20,25,30};
+int soma;
+for (int i=0;i<5;i++)
+{
+     soma += numeros[i];
+
+
+
+}
+  printf("\n\n \t %d ",soma);
+
+
+
+
+    return 0;
+}
